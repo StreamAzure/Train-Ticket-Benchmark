f4a6e80a77e8904fce93e4be89b667e21552020d
StreamAzure <1905105036@qq.com>
2024-03-28 19:18:24 +0800

    故障请求

--- auto-query/test.py
+++ auto-query/test.py
@@ -1,7 +1,7 @@
 import logging
 from time import sleep
 from queries import Query
-from scenarios import query_and_preserve
+from scenarios import *
 
 # login train-ticket and store the cookies
 url = 'http://localhost:8080'
@@ -12,10 +12,13 @@ if not q.login():
 print("login success")
 
 # execute scenario on current user
-while(True):
-    # query_and_preserve(q)
-    start = "Shang Hai"
-    end = "Nan Jing"
-    other_place_pair = (start, end)
-    trip_ids = q.query_normal_ticket(place_pair=other_place_pair)
-    sleep(1)
+# while(True):
+#     # query_and_preserve(q)
+#     start = "Shang Hai"
+#     end = "Nan Jing"
+#     other_place_pair = (start, end)
+#     trip_ids = q.query_normal_ticket(place_pair=other_place_pair)
+#     sleep(1)
+
+query_and_preserve(q)
+query_and_cancel(q)
